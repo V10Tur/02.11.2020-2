dd00ebfb8809c29fbff909561ddf767b2d601866
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:22:28 +0300

    Create 02.11.2020-2.java

---
+++ 02.11.2020-2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Program{
+
+    public static void main (String args[]){
+
+    }
+    void hello(){
+
+        System.out.println("Hello");
+    }
+    void welcome(){
+
+        System.out.println("Welcome to Java 10");
+    }
+}
